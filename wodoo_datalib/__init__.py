b1b99f30d5ea3013c2fd5060f52221f4c4ff18ac
Joshua <joshua_kreuder@outlook.de>
2022-11-17 18:19:40 +0000

    [BUMP] Package 0.0.3 to 0.0.4

--- wodoo_datalib/__init__.py
+++ wodoo_datalib/__init__.py
@@ -1,5 +1,5 @@
 """Wodoo Datalib."""
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 from .csv_tools import pandas_read_csv  # noqa
 from .dataset import Dataset  # noqa
 from .env_vars import ensure_env_var  # noqa
